bc17c9666d9612ed0ba04f980697d1bfae4f52ed
noeyoes <lsy021128@naver.com>
2024-07-05 02:20:02 +0900

    🐛fix: swagger 적용 시 문제 해결 (#28)

--- src/main/java/org/smlions/comfortplant/Security/Controller/AuthController.java
+++ src/main/java/org/smlions/comfortplant/Security/Controller/AuthController.java
@@ -1,5 +1,7 @@
 package org.smlions.comfortplant.Security.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.smlions.comfortplant.Security.dto.JwtDto;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "토큰 발급 API", description = "토큰 발급 API입니다.")
 public class AuthController {
 
     private final AuthService authService;
 
     //토큰 재발급 API
     @PostMapping("/reissue")
+    @Operation(method = "POST", summary = "토큰 재발급", description = "토큰 재발급. accessToken과 refreshToken을 body에 담아서 전송합니다.")
     public ResponseEntity<?> reissue(@RequestBody JwtDto jwtDto) {
 
         log.info("[ Auth Controller ] 토큰을 재발급합니다. ");
